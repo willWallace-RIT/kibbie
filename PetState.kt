273f9311593c0698f19f2d46efbe748b82001450
northbot <54906390+willWallace-RIT@users.noreply.github.com>
2026-04-14 08:19:04 -0400

    Create PetState.kt

---
+++ PetState.kt
@@ -0,0 +1,12 @@
+package com.example.virtualkbd
+
+class PetState(
+    var hunger: Int = 50,
+    var happiness: Int = 50,
+    var energy: Int = 50,
+    var x: Int = 64,
+    var y: Int = 64,
+    var dirX: Int = 1,
+    var dirY: Int = 0,
+    var animFrame: Int = 0
+)
